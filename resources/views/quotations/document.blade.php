db140234d2dd0af9b2267fafff01d66a9617efa0
dev.asman <asman@temasline.com>
2026-07-21 10:38:23 +0700

    template

--- resources/views/quotations/document.blade.php
+++ resources/views/quotations/document.blade.php
@@ -113,7 +113,7 @@
             </section>
             <section class="signatures">
                 <div><div class="signature-heading">Sincerely Yours,</div><div class="signature-space"></div><div class="signature-name">{{ $quotation->sender_name }}</div><div>{{ $quotation->sender_title }}</div></div>
-                <div><div class="signature-heading">Approved By,</div><div class="signature-space"></div><div class="signature-name">{{ $quotation->customer_name }}</div></div>
+                <div><div class="signature-heading">Approved By,</div><div class="signature-space"></div><div class="signature-name">{{ $quotation->attention_name ? $quotation->attention_name : $quotation->customer_name }}</div></div>
             </section>
         </div>
     </div>
